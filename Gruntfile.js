cbdb1bff69820ebd62ab70f46f7f116e7c61a318
Dbay <gdbaileyii@gmail.com>
2015-05-20 14:56:07 -0400

    began an ARIA best practice page

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,19 +69,6 @@ module.exports = function(grunt) {
       },
     },
 
-
-
-    // configure sass documentation --> grunt sassdoc
-    // sassdoc: {
-    //   default: {
-    //     src: 'scss',
-    //     dest: 'docs',
-    //     options: {
-    //       package: 'package.json'
-    //     }
-    //   }
-    // },
-
     // configure concatenation --> grunt concat
     concat: {
       dist: {
@@ -149,7 +136,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-browser-sync');
-  //grunt.loadNpmTasks('grunt-sassdoc');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
@@ -161,18 +147,3 @@ module.exports = function(grunt) {
   grunt.registerTask( 'build', [ 'imagemin','sass:prod' ] ); // optimize images, compress css
 
 };
-
-/*
-    Notes:
-
-    When registering a new Task we can also pass in any other registered Tasks.
-    e.g. grunt.registerTask('release', 'default requirejs'); // when running this task we also run the 'default' Task
-
-    We don't do this above as we would end up running `sass:dev` when we only want to run `sass:dist`
-    We could do it and `sass:dist` would run afterwards, but that means we're compiling sass twice which (although in our example quick) is extra compiling time.
-
-    To run specific sub tasks then use a colon, like so...
-    grunt sass:dev
-    grunt sass:dist
-
-*/
